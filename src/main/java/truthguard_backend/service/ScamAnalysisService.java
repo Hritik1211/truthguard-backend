64f9140b6c44f3e4870b7f0381010b1c4b881edd
Hritik Gehlot <hritikgehlot@Hritiks-MacBook-Air.local>
2026-05-15 00:06:13 +0530

    removed ollama completely

--- src/main/java/truthguard_backend/service/ScamAnalysisService.java
+++ src/main/java/truthguard_backend/service/ScamAnalysisService.java
@@ -1,6 +1,5 @@
 package truthguard_backend.service;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
 import truthguard_backend.dto.ScamAnalysisResponse;
@@ -10,42 +9,24 @@ import java.util.List;
 @Service
 public class ScamAnalysisService {
 
-    private final ChatClient chatClient;
-
-    public ScamAnalysisService(
-            ChatClient.Builder builder
-    ) {
-
-        this.chatClient = builder.build();
-    }
-
     public ScamAnalysisResponse analyzeText(String text) {
 
-        try {
+        ScamAnalysisResponse response =
+                new ScamAnalysisResponse();
 
-            ScamAnalysisResponse response =
-                    new ScamAnalysisResponse();
+        response.setScam(true);
 
-            response.setScam(true);
+        response.setRisk(90);
 
-            response.setRisk(90);
+        response.setCategory("Phishing");
 
-            response.setCategory("Phishing");
+        response.setReason(
+                List.of(
+                        "Urgency language detected",
+                        "Suspicious message"
+                )
+        );
 
-            response.setReason(
-                    List.of(
-                            "Urgency language detected",
-                            "Suspicious message"
-                    )
-            );
-
-            return response;
-
-        } catch (Exception e) {
-
-            throw new RuntimeException(
-                    e.getMessage()
-            );
-        }
+        return response;
     }
 }
