5ceccf0e93cd44eb2cffcb0ebdff2b94eece3284
Hritik Gehlot <hritikgehlot@Hritiks-MacBook-Air.local>
2026-05-14 10:25:49 +0530

    minimal backend deployment

---
+++ src/main/java/truthguard_backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package truthguard_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "TruthGuard Backend Running";
+    }
+}
